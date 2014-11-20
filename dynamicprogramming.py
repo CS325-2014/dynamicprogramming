6e7cad1c99b271fbc65f15d40d180d39fbaa2617
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-11-20 01:00:32 -0800

    some inputs and outputs

--- dynamicprogramming.py
+++ dynamicprogramming.py
@@ -43,6 +43,7 @@ class Grid():
         h[x][y] = self.values[x][y] + max(self.h_value(h, x-1, y),
           self.h_value(h, x, y-1), 0)
     self.h = h
+    print h
 
   def ending_point(self):
     (i, j) = (0, 0)
